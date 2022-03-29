0f6f32f2715d095b0063f2f5e9d3a9e5ba924f08
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-29 22:19:30 +0900

    [Update] config.ymlに書いた機能を追加

--- src/main/java/com/nekozouneko/nekopvp/nekopvp/util/timer.java
+++ src/main/java/com/nekozouneko/nekopvp/nekopvp/util/timer.java
@@ -1,11 +1,13 @@
 package com.nekozouneko.nekopvp.nekopvp.util;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.scoreboard.Scoreboard;
 
 public class timer implements Runnable {
 
@@ -28,6 +30,32 @@ public class timer implements Runnable {
                     p.sendTitle(" ", this.plugin.getConfig().getString("translate.start"), 5, 60, 5);
                     p.playSound(p, Sound.BLOCK_ANVIL_DESTROY, 1L, 1L);
                 }
+
+                Scoreboard score = Bukkit.getScoreboardManager().getMainScoreboard();
+                if (this.plugin.getConfig().getBoolean("team_one.teleport")) {
+                    for (Player p : getterUtil.getNameToPlayer(score.getTeam(this.plugin.getConfig().getString("team_one.id")).getEntries())) {
+                        p.teleport(new Location(p.getWorld(), this.plugin.getConfig().getDouble("team_one.x"), this.plugin.getConfig().getDouble("team_one.y"), this.plugin.getConfig().getDouble("team_one.z")));
+                    }
+                }
+
+                if (this.plugin.getConfig().getBoolean("team_two.teleport")) {
+                    for (Player p : getterUtil.getNameToPlayer(score.getTeam(this.plugin.getConfig().getString("team_two.id")).getEntries())) {
+                        p.teleport(new Location(p.getWorld(), this.plugin.getConfig().getDouble("team_two.x"), this.plugin.getConfig().getDouble("team_two.y"), this.plugin.getConfig().getDouble("team_two.z")));
+                    }
+                }
+
+                if (this.plugin.getConfig().getBoolean("team_one.enable_cmd")) {
+                    for (String cmd : this.plugin.getConfig().getStringList("team_one.commands")) {
+                        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), cmd);
+                    }
+                }
+
+                if (this.plugin.getConfig().getBoolean("team_two.enable_cmd")) {
+                    for (String cmd : this.plugin.getConfig().getStringList("team_two.commands")) {
+                        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), cmd);
+                    }
+                }
+
                 this.task.cancel();
             } else {
                 for (Player p:Bukkit.getOnlinePlayers()) {
