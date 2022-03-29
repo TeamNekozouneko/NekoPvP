62b9c1f2434584ec0471296c020334de24a9fe01
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-29 15:06:54 +0900

    [Update] countdownコマンド追加

--- src/main/java/com/nekozouneko/nekopvp/nekopvp/NekoPvP.java
+++ src/main/java/com/nekozouneko/nekopvp/nekopvp/NekoPvP.java
@@ -1,9 +1,6 @@
 package com.nekozouneko.nekopvp.nekopvp;
 
-import com.nekozouneko.nekopvp.nekopvp.cmd.mainCmd;
-import com.nekozouneko.nekopvp.nekopvp.cmd.randomizeCmd;
-import com.nekozouneko.nekopvp.nekopvp.cmd.startCmd;
-import com.nekozouneko.nekopvp.nekopvp.cmd.teambarCmd;
+import com.nekozouneko.nekopvp.nekopvp.cmd.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NekoPvP extends JavaPlugin {
@@ -20,11 +17,13 @@ public final class NekoPvP extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getResource("");
         saveDefaultConfig();
         getCommand("start").setExecutor(new startCmd());
         getCommand("nekopvp").setExecutor(new mainCmd());
         getCommand("randomize").setExecutor(new randomizeCmd());
         getCommand("teambar").setExecutor(new teambarCmd());
+        getCommand("countdown").setExecutor(new countdownCmd());
     }
 
     @Override
