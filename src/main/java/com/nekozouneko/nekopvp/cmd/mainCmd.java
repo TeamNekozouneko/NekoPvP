10d0fbc9491a03498d125ebd6eff04631acd4581
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-30 17:18:56 +0900

    [Update] & == §

--- src/main/java/com/nekozouneko/nekopvp/cmd/mainCmd.java
+++ src/main/java/com/nekozouneko/nekopvp/cmd/mainCmd.java
@@ -1,6 +1,7 @@
 package com.nekozouneko.nekopvp.cmd;
 
 import com.nekozouneko.nekopvp.NekoPvP;
+import com.nekozouneko.nekopvp.util.getterUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,6 +16,9 @@ public class mainCmd implements CommandExecutor, TabCompleter {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args[0].equalsIgnoreCase("reload")) {
             this.plugin.reloadConfig();
+            sender.sendMessage(getterUtil.toColorCode("&",this.plugin.getConfig().getString("translate.prefix"))+getterUtil.toColorCode("&", this.plugin.getConfig().getString("translate.reloaded")));
+        } else {
+            sender.sendMessage("NekoPvP");
         }
         return true;
     }
